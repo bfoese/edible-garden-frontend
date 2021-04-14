0ad258d837b76234f82aa1753e601c18e0b1d91f
Britt Foese <britt.foese@gmail.com>
2021-04-14 13:00:41 +0200

    chore(cleanup): remove unused stuff

--- projects/seed-sharing-app/src/app/seed-sharing/seed-sharing.module.ts
+++ projects/seed-sharing-app/src/app/seed-sharing/seed-sharing.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AngularSplitModule } from 'angular-split';
 
 import { HeaderModule } from '../feature/layout/header/header.module';
-import { SeedLayoutModule } from '../feature/layout/layout.module';
 import { SeedSharingRoutingModule } from './seed-sharing-routing.module';
 import { SeedSharingComponent } from './seed-sharing.component';
 
@@ -13,7 +12,6 @@ import { SeedSharingComponent } from './seed-sharing.component';
   imports: [
     CommonModule,
     SeedSharingRoutingModule,
-    SeedLayoutModule,
     HeaderModule,
     AngularSplitModule
   ]
